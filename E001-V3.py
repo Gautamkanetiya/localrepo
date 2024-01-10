c9c323a22fc92ffb2a1201dce6a9d07eaa621a7c
Gautamkanetiya <154489460+Gautamkanetiya@users.noreply.github.com>
2024-01-10 15:23:00 +0530

    Update E001-V3.py

--- E001-V3.py
+++ E001-V3.py
@@ -5,13 +5,8 @@ class Category:
         self.parent = parent
         self.products = []
 
-    def add_product(self, product):
-        self.products.append(product)
 
-    def generate_display_name(self):
-        if self.parent:
-            return f"{self.parent.generate_display_name()} > {self.name}"
-        return self.name
+
 
     def __str__(self):
         return f"{self.name} {self.code} {len(self.products)}"
@@ -59,11 +54,21 @@ class Product:
             raise ValueError(f"stock is not avilable")
 
     def display_details(self):
-        print(f"Product Name: {self.name} ({self.code})")
-        print(f"Category: {self.category.name}")
-        print(f"Price: {self.price}")
-        print(f"Stock at Locations: {self}")
+        print(self)
         print()
+    @classmethod
+    def print_product_movements(self,product):
+        movements = set(Movement.movements_by_product(product))
+
+
+    @classmethod
+    def print_stock_information(self,location_list, product_list):
+        for location in location_list:
+            print(f"{location.name}:")
+            location_products = [product for product in product_list if location in product.stock_at_locations]
+            for product in location_products:
+                print(f"  {product.name} ({product.code}): {product.stock_at_locations[location]}")
+            print()
 
 
 class Location:
@@ -107,41 +112,31 @@ loc3 = Location("Junagadh", 2003)
 loc4 = Location("Amreli", 2004)
 
 # Creating products
-product20 = Product("Product20", 1038, petrol, 500, {loc1: 50, loc3: 60, loc2: 70, loc4: 90})
-product21 = Product("Product21", 1039, petrol, 600, {loc2: 70, loc1: 10, loc3: 40, loc4: 80})
-product22 = Product("Product22", 1040, petrol, 300, {loc3: 45, loc4: 41, loc2: 90, loc1: 85})
-product23 = Product("Product23", 1041, petrol, 100, {loc4: 35, loc3: 43, loc1: 87, loc2: 74})
-product24 = Product("Product24", 1042, petrol, 900, {loc1: 40, loc4: 54, loc3: 84, loc2: 96})
-
+product20 = Product("Product20", 1038, petrol, 500, {loc1: 50})
+product21 = Product("Product21", 1039, petrol, 600, {loc2: 70})
+product22 = Product("Product22", 1040, petrol, 300, {loc3: 45})
+product23 = Product("Product23", 1041, petrol, 100, {loc4: 35})
+product24 = Product("Product24", 1042, petrol, 900, {loc1: 40})
+product_list= [product20, product21, product22, product23, product24]
+location_list=[loc1,loc2,loc3,loc4]
 # Displaying product details
 print("------------______________________________display details__________________------------------------------")
-for product in [product20, product21, product22, product23, product24]:
+for product in product_list:
     product.display_details()
 
 print()
 
 # Displaying product list by location
-print("--------------------------------------product list by location -------------------------------------------")
-locations = [loc1, loc2, loc3, loc4]
-for location in locations:
-    print(f"{location.name}:")
-    location_products = [product for product in [product20, product21, product22, product23, product24] if location in product.stock_at_locations]
-    for product in location_products:
-        print(f"  {product.name} ({product.code}): {product.stock_at_locations[location]}")
-    print()
+
 
 # Moving products from one location to another
-product20.move(loc1, loc2, 10)
+product20.move(loc1, loc2,10)
 product21.move(loc2, loc3, 5)
 product22.move(loc3, loc4, 15)
-product23.move(loc4, loc1, 20)
-product24.move(loc3, loc1, 8)
+product23.move(loc4, loc1, 9)
+product24.move(loc1, loc3, 8)
 
 # Displaying movements of each product
 print("---------------------------------------------movements details -------------------------------------------")
-for product in [product20, product21, product22, product23, product24]:
-    movements = set(Movement.movements_by_product(product))
-    print(f"Product: {product.name} ({product.code})")
-    for movement in movements:
-        print(f"  {movement}")
-    print()
+Product.print_stock_information(location_list,product_list)
+Product.print_product_movements(product)
